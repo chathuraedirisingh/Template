6995d72690c207bdd46f3577dbf0824adf2ed145
chathuraedirisingh <arkxstyler.forum@gmail.com>
2019-11-26 14:57:57 +0530

    first commit

---
+++ .history/src/screens/ProfileScreen_20191126145040.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Text, View, Button } from 'react-native'
+
+export default class ProfileScreen extends Component {
+    static navigationOptions = {
+        title: 'Profile',
+      };
+      render() {
+        const {navigate} = this.props.navigation;
+        return (
+          <Button
+            title="Go to Jane's profile"
+            onPress={() => navigate('Home', {name: 'Jane'})}
+          />
+        );
+      }
+}
